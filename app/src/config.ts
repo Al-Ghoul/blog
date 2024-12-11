487ac0645f238a6ab9c68574ecf30d313d5fdda0
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2024-12-11 11:21:06 +0200

    feat: tailor the blog towards me

--- app/src/config.ts
+++ app/src/config.ts
@@ -7,16 +7,16 @@ import type {
 import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
-  title: 'Fuwari',
+  title: 'AlGhoul\'s Blog',
   subtitle: 'Demo Site',
-  lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
+  lang: 'en',         // 'en', 'ar'
   themeColor: {
-    hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+    hue: 270,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
     fixed: false,     // Hide the theme color picker for visitors
   },
   banner: {
-    enable: false,
-    src: 'assets/images/demo-banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+    enable: true,
+    src: 'assets/images/banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
     position: 'center',      // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
     credit: {
       enable: false,         // Display the credit text of the banner image
@@ -44,33 +44,48 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.About,
     {
       name: 'GitHub',
-      url: 'https://github.com/saicaca/fuwari',     // Internal links should not include the base path, as it is automatically added
+      url: 'https://github.com/Al-Ghoul/blog',     // Internal links should not include the base path, as it is automatically added
       external: true,                               // Show an external link icon and will open in a new tab
     },
   ],
 }
 
 export const profileConfig: ProfileConfig = {
-  avatar: 'assets/images/demo-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'Lorem Ipsum',
-  bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  avatar: 'https://avatars.githubusercontent.com/u/29547307',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  name: 'AlGhoul',
+   bio: "مادام في القلبِ نية و في العقل رأيٌ و في النفس عزمٌ و في الوقتِ متسع، فكل شئٍ يمكن تداركهْ.",
   links: [
     {
-      name: 'Twitter',
-      icon: 'fa6-brands:twitter',       // Visit https://icones.js.org/ for icon codes
-                                        // You will need to install the corresponding icon set if it's not already included
-                                        // `pnpm add @iconify-json/<icon-set-name>`
-      url: 'https://twitter.com',
+      name: "Twitter",
+      icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
+      // You will need to install the corresponding icon set if it's not already included
+      // `pnpm add @iconify-json/<icon-set-name>`
+      url: "https://twitter.com/abdo_alghoul",
     },
     {
-      name: 'Steam',
-      icon: 'fa6-brands:steam',
-      url: 'https://store.steampowered.com',
+      name: "Instagram",
+      icon: "fa6-brands:instagram",
+      url: "https://www.instagram.com/abdo.alghoul/",
     },
     {
-      name: 'GitHub',
-      icon: 'fa6-brands:github',
-      url: 'https://github.com/saicaca/fuwari',
+      name: "TikTok",
+      icon: "fa6-brands:tiktok",
+      url: "https://www.tiktok.com/@abdo.alghoul",
+    },
+    {
+      name: "Youtube",
+      icon: "fa6-brands:youtube",
+      url: "https://www.youtube.com/@Abdo.AlGhoul",
+    },
+    {
+      name: "Paypal",
+      icon: "fa6-brands:paypal",
+      url: "https://paypal.me/AlGhouul?country.x=EG&locale.x=en_US",
+    },
+    {
+      name: "GitHub",
+      icon: "fa6-brands:github",
+      url: "https://github.com/Al-Ghoul",
     },
   ],
 }
